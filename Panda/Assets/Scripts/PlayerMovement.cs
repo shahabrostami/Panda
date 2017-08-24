1c719a7cf945c40bf456a0a83c2dd1e3a38e69de
Shahab Rostami <shbrostami@gmail.com>
2017-08-24 19:39:22 +0100

    Input + Movement
    Animation walking and running on protoype plane

--- Panda/Assets/Scripts/PlayerMovement.cs
+++ Panda/Assets/Scripts/PlayerMovement.cs
@@ -4,25 +4,42 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
-    private Rigidbody rb;
-    private Vector3 objCentre;
-    
-    public float speed;
-	// Use this for initialization
-	void Start () {
-        rb = GetComponent<Rigidbody>();
-        Renderer renderer = GetComponent<Renderer>();
-        Debug.Log(renderer.bounds.size);
-        objCentre = new Vector3(0.5f, 0f, 0.5f);
+    public float walkSpeed = 2;
+    public float runSpeed = 6;
+    public float turnSpeedTime = 0.2f;
+    float turnSmoothVelocity;
 
+    public float speedSmoothTime = 0.1f;
+    float speedSmoothVelocity;
+
+    float currentSpeed;
+
+    Animator animator;
+    
+	void Start () {
+        animator = GetComponent<Animator>();
     }
 	
 	void FixedUpdate ()
     {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
-        
-        transform.Translate(moveHorizontal*speed *Time.deltaTime, 0f, moveVertical * speed * Time.deltaTime);
-        //rb.AddForce(momentum * speed);
+        Vector2 input = new Vector2(moveHorizontal, moveVertical);
+        Vector2 inputDir = input.normalized;
+
+        if (inputDir != Vector2.zero)
+        {
+            float targetRotation = Mathf.Atan2(inputDir.x, inputDir.y) * Mathf.Rad2Deg;
+            transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref turnSmoothVelocity, turnSpeedTime);
+        }
+
+        bool running = Input.GetKey(KeyCode.LeftShift);
+        float targetSpeed = ((running) ? runSpeed : walkSpeed) * inputDir.magnitude;
+        currentSpeed = Mathf.SmoothDamp(currentSpeed, targetSpeed, ref speedSmoothVelocity, speedSmoothTime);
+
+        transform.Translate(transform.forward * currentSpeed * Time.deltaTime, Space.World);
+
+        float animationSpeedPercent = ((running) ? 1 : .5f) * inputDir.magnitude;
+        animator.SetFloat("speedPercent", animationSpeedPercent, speedSmoothTime, Time.deltaTime);
     }
 }
