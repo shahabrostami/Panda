ee4c5144e86063d162750b66298d3db246d03dbf
Shahab Rostami <shbrostami@gmail.com>
2017-08-29 10:52:43 +0100

    Collisions
    Collisions and obstacles, player movment now includes jumping

--- Panda/Assets/Scripts/PlayerMovement.cs
+++ Panda/Assets/Scripts/PlayerMovement.cs
@@ -7,41 +7,90 @@ public class PlayerMovement : MonoBehaviour {
     public float walkSpeed = 2;
     public float runSpeed = 6;
     public float turnSpeedTime = 0.2f;
-    float turnSmoothVelocity;
+    public float gravity = -12;
+    public float jumpHeight = 1;
+    [Range(0,1)]
+    public float airControlPercent;
 
+    float turnSmoothVelocity;
     public float speedSmoothTime = 0.1f;
     float speedSmoothVelocity;
-
     float currentSpeed;
+    float velocityY;
 
     Animator animator;
     Transform cameraT;
+    CharacterController controller;
     
 	void Start () {
         animator = GetComponent<Animator>();
         cameraT = Camera.main.transform;
+        controller = GetComponent<CharacterController>();
     }
 	
 	void FixedUpdate ()
     {
+        // input section
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
         Vector2 input = new Vector2(moveHorizontal, moveVertical);
         Vector2 inputDir = input.normalized;
 
+        bool running = Input.GetKey(KeyCode.LeftShift);
+        DoMove(inputDir, running);
+
+        if(Input.GetKeyDown(KeyCode.Space))
+            DoJump();
+
+        // animator
+        float animationSpeedPercent = ((running) ? currentSpeed / runSpeed : currentSpeed / walkSpeed * 0.5f);
+        animator.SetFloat("speedPercent", animationSpeedPercent, speedSmoothTime, Time.deltaTime);
+    }
+
+    void DoJump()
+    {
+        if(controller.isGrounded)
+        {
+            float jumpVelociy = Mathf.Sqrt(-2 * gravity * jumpHeight);
+            velocityY = jumpVelociy;
+        }
+    }
+
+    float GetModifiedSmoothTime(float smoothTime)
+    {
+        if(controller.isGrounded)
+        {
+            return smoothTime;
+        }
+
+        if (airControlPercent == 0)
+            return float.MaxValue;
+
+        return smoothTime / airControlPercent;
+    }
+
+    void DoMove(Vector2 inputDir, bool running)
+    {
         if (inputDir != Vector2.zero)
         {
             float targetRotation = Mathf.Atan2(inputDir.x, inputDir.y) * Mathf.Rad2Deg + cameraT.eulerAngles.y;
-            transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref turnSmoothVelocity, turnSpeedTime);
+            transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation, ref turnSmoothVelocity, GetModifiedSmoothTime(turnSpeedTime));
         }
-
-        bool running = Input.GetKey(KeyCode.LeftShift);
+        
         float targetSpeed = ((running) ? runSpeed : walkSpeed) * inputDir.magnitude;
-        currentSpeed = Mathf.SmoothDamp(currentSpeed, targetSpeed, ref speedSmoothVelocity, speedSmoothTime);
+        currentSpeed = Mathf.SmoothDamp(currentSpeed, targetSpeed, ref speedSmoothVelocity, GetModifiedSmoothTime(speedSmoothTime));
 
-        transform.Translate(transform.forward * currentSpeed * Time.deltaTime, Space.World);
+        // transform.Translate(transform.forward * currentSpeed * Time.deltaTime, Space.World);
 
-        float animationSpeedPercent = ((running) ? 1 : .5f) * inputDir.magnitude;
-        animator.SetFloat("speedPercent", animationSpeedPercent, speedSmoothTime, Time.deltaTime);
+        velocityY += Time.deltaTime * gravity;
+        Vector3 velocity = transform.forward * currentSpeed + Vector3.up * velocityY;
+
+        controller.Move(velocity * Time.deltaTime);
+
+        currentSpeed = new Vector2(controller.velocity.x, controller.velocity.z).magnitude;
+
+
+        if (controller.isGrounded)
+            velocityY = 0;
     }
 }
